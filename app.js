943cbcdb2a87b02643c2b284de4d25cb1812107e
Aayush-dot <59513001+Aayush-dot@users.noreply.github.com>
2020-03-10 01:16:19 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,14 +1,58 @@
 const express = require('express');
+const request = require('request');
 const bodyParser = require('body-parser');
 const { getChart } = require('billboard-top-100');
-var request = require('request');
-
+const giveMeAJoke = require('give-me-a-joke');
 const app=express();
-app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
+app.get("/",function(req,res){
+  res.render("landing");
+});
+
+// app.get("/",function(req,res){
+//   res.render("landing");
+// });
 
 
+
+app.post("/home",function(req,res){
+  res.render("home");
+});
+
+var word,val;
+
+app.post("/dic",function(req,res){
+  res.render("dicindex");
+});
+
+
+
+app.post("/find",function(req,res){
+  word=req.body.word;
+  // console.log(word);
+  let url="https://www.dictionaryapi.com/api/v3/references/collegiate/json/" + word + "?key="+"1f1f3a1c-0d7a-441c-a5a0-46094d35f54a";
+  request(url,function(err,res1,body){
+    // console.log(res1.body);
+    val = JSON.parse(res1.body);
+    console.log(val);
+    if(val[0] == undefined)
+    {
+      res.render("failure");
+    }
+    else if(val[0].meta == undefined)
+    {
+      res.render("failure");
+    }
+    else{
+    res.render("find",{
+      searched: word,
+      received: val
+    });
+  }
+});
+});
 
 var d = new Date();
 var pastDate = d.getDate() - 7;
@@ -24,7 +68,7 @@ d.setDate(pastDate);
 
     var l= year+'-'+month+'-'+day;
 
-  app.get("/",function(req,res){
+  app.post("/bill",function(req,res){
 getChart('hot-100',l, (err, chart) => {
   if (err) console.log(err);
   else
@@ -35,23 +79,18 @@ getChart('hot-100',l, (err, chart) => {
     });
 
   }
-  console.log(chart.week) // prints the week of the chart in the date format YYYY-MM-DD
-  console.log(chart.previousWeek.url) // prints the URL of the previous week's chart
-  console.log(chart.previousWeek.date) // prints the date of the previous week's chart in the date format YYYY-MM-DD
-  console.log(chart.nextWeek.url) // prints the URL of the next week's chart
-  console.log(chart.nextWeek.date) // prints the date of the next week's chart in the date format YYYY-MM-DD
-  console.log(chart.songs); // prints array of top 100 songs for week of August 27, 2016
-  console.log(chart.songs[3]); // prints song with rank: 4 for week of August 27, 2016
-  console.log(chart.songs[0].title); // prints title of top song for week of August 27, 2016
-  console.log(chart.songs[0].artist); // prints artist of top songs for week of August 27, 2016
-  console.log(chart.songs[0].rank) // prints rank of top song (1) for week of August 27, 2016
-  console.log(chart.songs[0].cover) // prints URL for Billboard cover image of top song for week of Aug
 
 });
   });
 
+app.post("/joke",function(req,res){
+  giveMeAJoke.getRandomDadJoke (function(joke) {
+  res.render("joke",{
+    joke: joke
+  });
+});
+});
 
-
-  app.listen("3000",function(){
+app.listen("3000",function(){
   console.log("Listening at port 3000");
 })
